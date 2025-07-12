8ad36e501e8e9376d66d976337687aa1c970edc6
Mahin-Rajput <mahinvrajput999@gmail.com>
2025-07-12 22:54:48 +0100

    added Exp Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from './components/navbar'
 import './App.css'
 import Binfo from './components/Binfo'
+import Experience from './components/Experience'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
      <Navbar/>
      <Binfo/>
+     <Experience/>
        
     </>
   )
